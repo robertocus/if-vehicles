fecd6cec81ed41c10c669886782d6aef32f82c62
robo <robert.jansson@gmail.com>
2025-06-29 14:14:40 +0200

    da code

--- src/main/java/com/robo/vehicles/domain/VehicleFinder.java
+++ src/main/java/com/robo/vehicles/domain/VehicleFinder.java
@@ -7,6 +7,10 @@ import java.util.Optional;
 
 @AllArgsConstructor
 public class VehicleFinder {
+    // Detta är en så kallad "Service" men då jag ogillar Service klasser så har jag en egen Finder som inte får en massa
+    // extra beroenden beroende på service metoden som en traditionell "Service" och du kan skapa andra specifika logik klasser i domänen.
+    // VehicleFinder(VehicleService) och Vehicle är båda domän.
+    // Domän klasser har inte beroende till några andra paket. infrastructure, api, config kan alla referera domän.
     @NonNull
     private VehicleRepository vehicleRepository;
 
