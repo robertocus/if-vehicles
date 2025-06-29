22b560c7a0fecc52fc9f923265c8f6bf084a796a
robo <robert.jansson@gmail.com>
2025-06-29 17:56:00 +0200

    fixed unit test

--- src/test/java/com/robo/vehicles/api/controller/VehiclesControllerTest.java
+++ src/test/java/com/robo/vehicles/api/controller/VehiclesControllerTest.java
@@ -5,8 +5,8 @@ import com.robo.vehicles.domain.VehicleFinder;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Optional;
@@ -21,7 +21,7 @@ class VehiclesControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private VehicleFinder vehicleFinder;
 
     @Test
